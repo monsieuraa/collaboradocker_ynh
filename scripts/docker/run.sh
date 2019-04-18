aae98012e319c244120436ed12ba082eea580932
czen <taccessviolation@gmail.com>
2019-04-19 02:32:58 +0300

    ADD: user and password for admin interface

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -19,7 +19,7 @@ server_name=$(ynh_app_setting_get "$app" domain)
 
 options="-p $port:9980 -e domain=$domaines -e server_name=$server_name --cap-add MKNOD"
 
-docker run -d --name=$app --restart always $options $image 1>&2
+docker run -d --name=$app -e "username=admin" -e "password=72nMyw" --restart always $options $image 1>&2
 RT=$?
 
 sleep 20
